b2a7524e476f8faaf59333bcba3bc3119cced7b7
James Westfall <james.westfall@bouvet.no>
2019-02-12 13:38:38 +0100

    Test for oracle string sql

--- Udir.XmlDataImport.Test/XmlDataImportOraTest.cs
+++ Udir.XmlDataImport.Test/XmlDataImportOraTest.cs
@@ -32,7 +32,7 @@ namespace Udir.XmlDataImport.Test
         [ClassCleanup]
         public static void Cleanup()
         {
-           _xmlInsert.Dispose();
+          _xmlInsert.Dispose();
         }
 
         [TestMethod]
@@ -67,6 +67,49 @@ namespace Udir.XmlDataImport.Test
 
             Assert.AreEqual(_maxSalary, int.Parse(ageValue.ToString()), "Failed to insert int variable value from code when" +
                 " insert of Employees.xmld ran.");
+        }
+
+        [TestMethod]
+        public void must_insert_string_xml()
+        {
+            var lastName = "Barns";
+            string actualLastName;
+            using (var insert = new XmlInsert(
+                true,
+                new Dictionary<string, object>
+                {
+                    {"lastName", lastName }
+                },
+                "OracleTest",
+                "Oracle",
+                $@"<?xml version='1.0' encoding='utf-8' ?>
+                    <Root>
+                        <Setup>
+                            DELETE FROM EMPLOYEES WHERE LAST_NAME = '{lastName}';                           
+                            DELETE FROM JOBS WHERE JOB_ID = 'BLA_BLA';                           
+                        </Setup>
+                        <JOBS>
+                            <JOB_ID>BLA_BLA</JOB_ID>
+                            <JOB_TITLE>Bla bla job</JOB_TITLE>
+                            <MIN_SALARY>4200</MIN_SALARY>
+                            <MAX_SALARY>4200</MAX_SALARY>
+                        </JOBS>
+                        <EMPLOYEES>
+                            <EMPLOYEE_ID>(EMPLOYEES_SEQ.NextVal)</EMPLOYEE_ID>
+                            <FIRST_NAME>Bob</FIRST_NAME>
+                            <LAST_NAME>variable('lastName')</LAST_NAME>
+                            <EMAIL>bob@barns.no</EMAIL>
+                            <PHONE_NUMBER>555-1234</PHONE_NUMBER>
+                            <HIRE_DATE>TO_DATE('2003/07/09', 'yyyy/mm/dd')</HIRE_DATE>
+                            <JOB_ID>BLA_BLA</JOB_ID>
+                          </EMPLOYEES>    
+                    </Root>"))
+            {
+                actualLastName = insert.DataContext
+                    .ExecuteScalar($"SELECT LAST_NAME FROM EMPLOYEES WHERE LAST_NAME = '{lastName}'").ToString();
+            }
+
+            Assert.AreEqual(actualLastName, lastName);
         }
     }
 }
